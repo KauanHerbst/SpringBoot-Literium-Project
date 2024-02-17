22ed959af968a891afe930e1a6140d5bd6e9d4f6
KauanHerbst <kauanherbst7@gmail.com>
2024-02-17 18:46:55 -0300

    Update Config

--- src/main/java/com/herbst/literium/config/Config.java
+++ src/main/java/com/herbst/literium/config/Config.java
@@ -1,10 +1,14 @@
 package com.herbst.literium.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.herbst.literium.repositories", entityManagerFactoryRef = "jpaSharedEM_entityManagerFactory")
+@EntityScan(basePackages = "com.herbst.literium.entities")
 public class Config {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
